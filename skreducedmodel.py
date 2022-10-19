d3045dfb9294275d3539a39c5a01dbe20b21c7b1
Franco <fcerino@mi.unc.edu.ar>
2022-10-19 18:20:20 -0300

    subida ipynb y modifs a skreducedmodel.py

--- skreducedmodel.py
+++ skreducedmodel.py
@@ -152,45 +152,78 @@ class Basis:
 
 class ReducedModel:
     # required performance metrics: prediction time and accuracy of the model.
+    def __init__(self,
+                regression_method = None,
+                seed_global_rb = 0,
+                greedy_tol = 1e-12,
+                lmax = 0,
+                nmax = np.inf,
+                normalize = False,
+                all_training_set_for_reg = False,
+                integration_rule = "riemann"
+                ):
 
+        # the default one can be Least Squares or Splines (like arby)
+        self.regression_method = regression_method
+        self.seed_global_rb = seed_global_rb  # the default seed is the first of the array "parameters"
+        self.greedy_tol = greedy_tol
+        self.lmax = lmax
+        self.nmax = nmax
+        self.normalize = normalize
+        self.all_training_set_for_reg = all_training_set_for_reg
+        self.integration_rule = integration_rule
+        
     # fit stage is offline.
     def fit(
-            # array N x d. N parameters. Each parameter is d dim.
-            parameters,
+            self,
+            parameters,  # array N x d. N parameters. Each parameter is d dim.
             # array N x t. N train functions w/ values at t phys_points.
-            training_set,
-            # array 1 x t. physical_points
-            physical_points,
-            # the default one can be Least Squares or Splines (like arby)
-            regression_method=None,
-            # the default seed is the first of the array "parameters"
-            seed_global_rb=0,
-            greedy_tol=1e-12,
-            lmax=0,
-            nmax=np.inf,
-            normalize=True,
-            use_all_training_set_for_regressions=False
+            training_set,  
+            physical_points,  # array 1 x t. physical_points
             ):
-
         # do parameter compression (find reduced basis RB).
         # can be multiple RB's in case of lmax>0.
         # do time compression, finding empirical times T_i.
-
         # regression stage: each regression has its own hyp-tuning.
         # if real training set:
         #     fit one regression for each T_i (map f:R^d->R)
         # elif complex training set:
         #     decompose complex data in phase and amplitude
         #     fit two regressions for each T_i (maps f:R^d->R)
-        pass
+
+        rb = self.reduced_basis(
+                           #  self,  # [fc] por que funciona comentando el self?
+                           training_set,
+                           parameters,
+                           physical_points,
+                           greedy_tol = self.greedy_tol,
+                           lmax = self.lmax,
+                           nmax = self.nmax,
+                           seed_global_rb = self.seed_global_rb,
+                           integration_rule = self.integration_rule,
+                           normalize = self.normalize
+                           )
+
+        return rb  # [fc] ver la salida que corresponde
 
     # predict stage is online (must be as fast as possible).
-    def predict(parameters):
+    def predict(
+                self,
+                parameters,
+                ):
         # search model built on subspace corresponding to parameter
         # compute prediction
         # return prediction
         pass
-    
+
+    def project(
+               self,
+               parameters,
+               test_set,
+               ):
+
+        pass
+        
     # comenzamos la implementacion de reduced_basis
     # la idea es acoplar esto al método fit.
     def reduced_basis(
@@ -198,10 +231,10 @@ class ReducedModel:
             training_set,
             parameters,
             physical_points,
-            greedy_tol=1e-12,
+            greedy_tol,
             lmax=0,
             nmax=np.inf,
-            seed_rb=0,
+            seed_global_rb=0,
             integration_rule="riemann",
             normalize=False
             ) -> RB:
@@ -315,7 +348,7 @@ class ReducedModel:
             )
 
             # seed
-            next_index = seed_rb
+            next_index = seed_global_rb
             seed = training_set[next_index]
 
             while next_index < Ntrain - 1:
