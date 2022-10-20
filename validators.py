3efd74cbe704f8b6b7e59a856ca8c778c4157e79
AgusMRM <agusrodriguezmedrano@gmail.com>
2022-10-20 18:03:14 -0300

    agregue validators

---
+++ validators.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def validate_parameters(input):
+    if not isinstance(input,np.ndarray):
+        raise TypeError("The input parameters must be a numpy array."
+        "Got instead type {}".format(type(input))
+        )
+
+def validate_training_set(input):
+    if not isinstance(input,np.ndarray):
+        raise TypeError("The input training_set must be a numpy array."
+        "Got instead type {}".format(type(input))
+        )
+
+def validate_physical_points(input):
+    if not isinstance(input,np.ndarray):
+        raise TypeError("The input physical_points must be a numpy array."
+        "Got instead type {}".format(type(input))
+        )
