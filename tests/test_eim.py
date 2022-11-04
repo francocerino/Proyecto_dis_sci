b842c1f2922c6a15f221c868631b9471d5265b4d
AgusMRM <agusrodriguezmedrano@gmail.com>
2022-11-04 15:10:02 -0300

    agregue el eim

---
+++ tests/test_eim.py
@@ -0,0 +1,56 @@
+from skreducedmodel.skeim import EIM
+
+from scipy.integrate import odeint
+
+from skreducedmodel.reducedbasis import ReducedBasis
+
+import numpy as np 
+
+import arby
+
+
+def pend(y, t, b, λ):
+    θ, ω = y
+    dydt = [ω, -b*ω - λ*np.sin(θ)]
+
+    return dydt
+
+
+def test_EIMFit():
+
+    b = 0.2
+    y0 = [np.pi/2, 0.]
+
+    param = np.linspace(1, 5, 101)
+    times = np.linspace(0, 50, 1001)
+
+    training = []
+    for λ in param:
+        sol = odeint(pend, y0, times, (b, λ))
+        training.append(sol[:, 0])
+
+    training_set = np.array(training)
+    parameters = param
+    physical_points = times
+    nmax = 10
+
+    model = ReducedBasis(
+                     seed_global_rb=0,
+                     greedy_tol=1e-10,
+                     lmax=1,
+                     normalize=False
+                     )
+
+    model.fit(
+               training_set=training_set,
+               parameters=parameters,
+               physical_points=physical_points)
+
+    ti = EIM(reduced_base = model)
+    ti.fit
+
+    print(ti.nodes)
+
+    assert ti.nodes[0] == 0
+    assert ti.nodes[5] == 167
+    assert ti.nodes[19] == 816
